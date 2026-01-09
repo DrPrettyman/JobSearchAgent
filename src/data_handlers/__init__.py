7de6647728d4cb09a626a87d2a74c01c01df7fdd
Joshua Prettyman <j.prettyman@icloud.com>
2026-01-09 01:25:08 +0100

    import fixes

--- src/data_handlers/__init__.py
+++ src/data_handlers/__init__.py
@@ -1,9 +1,11 @@
 from .user_data import User
-from .query_data import SearchQueries
-from .jobs_data import Jobs
+from .query_data import SearchQueries, SearchQuery
+from .jobs_data import Jobs, Job
 
 __all__ = [
     "User", 
     "SearchQueries", 
-    "Jobs"
+    "SearchQuery",
+    "Jobs",
+    "Job",
     ]
